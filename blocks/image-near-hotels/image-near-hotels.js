a52130933e59c5bf10dd9c38ea03f8205858e9dc
alvaro <aarce@adobe.com>
2023-02-13 21:19:30 +0100

    Fixed lint issues and added icons from chFont

--- blocks/image-near-hotels/image-near-hotels.js
+++ blocks/image-near-hotels/image-near-hotels.js
@@ -1,33 +1,33 @@
-
 function createImageTeaser(block) {
-  const newImageTeaser = document.createElement('div');
-  /*
-    newTableTechnoForMobile.classList.add('table-techno-mobile');
-    block.parentElement.appendChild(newTableTechnoForMobile);
-  */
-    const titleTables = [];
-    const imageTables = [];
-    const descriptionTables = [];
-  
-    const rows = block.querySelectorAll('.image-near-hotels > div');
-    rows[1].style.display='none';
-    rows.forEach((element, i) => {
-      console.log(element);
-    });
-  /*
-    titleTables.forEach((theTitle, i) => {
-      const textInDiv = titleTables[i].textContent;
-      titleTables[i].textContent = '';
-      const titleH1 = document.createElement('h1');
-      titleH1.textContent = textInDiv;
-      titleTables[i].appendChild(titleH1);
-  
-      newTableTechnoForMobile.appendChild(titleTables[i]);
-      newTableTechnoForMobile.appendChild(imageTables[i]);
-      newTableTechnoForMobile.appendChild(descriptionTables[i]);
-    });*/
-  }
-  
-  export default async function decorate(block) {
-    createImageTeaser(block);
-  }
+  const rows = block.querySelectorAll('.image-near-hotels > div');
+  rows[1].style.display = 'none';
+  rows[1].className = '';
+
+  const linkInImage = rows[1].getElementsByTagName('div');
+  const divForLink = document.createElement('div');
+  divForLink.className = 'text-in-image';
+  divForLink.innerHTML = linkInImage[0].firstChild.outerHTML;
+
+  const arrowRight = document.createElement('span');
+  arrowRight.className = 'ch-icon-arrow-right';
+  arrowRight.innerHTML = '&nbsp;';
+
+  const textWithArrow = divForLink.innerHTML.trim();
+  const linkWithArrow = textWithArrow.slice(0, (textWithArrow.length) - 4)
+  + arrowRight.outerHTML + textWithArrow.slice((textWithArrow.length) - 4);
+  divForLink.innerHTML = linkWithArrow;
+  const picture = block.querySelectorAll('.image-near-hotels > div > div');
+  picture[0].appendChild(divForLink);
+
+  const mapMarker = document.createElement('span');
+  mapMarker.className = 'ch-icon-map-marker fs-20';
+  mapMarker.innerHTML = '&nbsp;';
+
+  const textAside = rows[2].innerHTML.trim();
+  const divWithSpan = textAside.slice(0, 5) + mapMarker.outerHTML + textAside.slice(5);
+  rows[2].innerHTML = divWithSpan;
+}
+
+export default async function decorate(block) {
+  createImageTeaser(block);
+}
