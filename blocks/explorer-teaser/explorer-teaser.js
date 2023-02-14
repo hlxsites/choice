7313fefea10ac72ba254ffd6202fb042b878bbca
Damian Zehnder <zehnder@adobe.com>
2023-02-14 14:21:57 +0100

    adding back to top button

---
+++ blocks/explorer-teaser/explorer-teaser.js
@@ -0,0 +1,6 @@
+export default function decorate(block) {
+  const backToTop = block.querySelector('.explorer-teaser > div > div:nth-child(2) p:last-child a.button ');
+  if (backToTop.href.includes('#top')) {
+    backToTop.href = '#top';
+  }
+}
